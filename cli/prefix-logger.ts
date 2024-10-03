8e661a7d51ee73b33e51c4871471354c894a3e5a
Michael Tyson <myty@users.noreply.github.com>
2024-10-02 22:23:23 -0400

    Refactor logging in CLI main.ts and add PrefixLogger class (#4)
    - Remove unused imports and variables
    - Replace console.log and console.error calls with PrefixLogger class in CLI main.ts
    - Add PrefixLogger class to provide logging with a prefix in CLI prefix-logger.ts

---
+++ cli/prefix-logger.ts
@@ -0,0 +1,40 @@
+import * as colors from "@std/fmt/colors";
+import { randomColor } from "./colors.ts";
+
+export class PrefixLogger {
+  private readonly decoder = new TextDecoder("utf-8");
+
+  readonly prefix: string;
+
+  constructor(prefix: string) {
+    const consoleColor = colors[randomColor()] as (str: string) => string;
+    this.prefix = consoleColor(
+      `${prefix}:`,
+    );
+  }
+
+  log(message: string | Uint8Array) {
+    this.console(console.log, message);
+  }
+
+  error(message: string | Uint8Array) {
+    this.console(console.error, message);
+  }
+
+  private console(
+    consoleFunc: (...args: unknown[]) => void,
+    message: string | Uint8Array,
+  ) {
+    const decoded = typeof message === "string"
+      ? message
+      : this.decoder.decode(message, { stream: true });
+
+    for (const line of decoded.split("\n")) {
+      if (!line) {
+        continue;
+      }
+
+      consoleFunc(`${this.prefix} ${line}`);
+    }
+  }
+}
