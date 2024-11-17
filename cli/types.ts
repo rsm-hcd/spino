5cb98c806bfe2afd4f9e84f2b9e4320f2b7e21ee
Allen Thich <allenthich@gmail.com>
2024-11-17 16:31:15 +0900

    test: add main cli entry point tests

--- cli/types.ts
+++ cli/types.ts
@@ -3,6 +3,7 @@ import type { displayHelpCommand } from "./commands/help.ts";
 import type { runTasksCommand } from "./commands/tasks.ts";
 import type { upgradeCommand } from "./commands/upgrade.ts";
 import type { listCommand } from "./commands/list.ts";
+import type { Spy } from "@std/testing/mock";
 
 export interface MainCliDependencies {
   parseCommands: typeof parseCommands;
@@ -10,6 +11,14 @@ export interface MainCliDependencies {
   runTasksCommand: typeof runTasksCommand;
   upgradeCommand: typeof upgradeCommand;
   listCommand: typeof listCommand;
+}
+
+export interface MainCliSpyDependencies {
+  parseCommands: Spy<typeof parseCommands>;
+  displayHelpCommand: Spy<typeof displayHelpCommand>;
+  runTasksCommand: Spy<typeof runTasksCommand>;
+  upgradeCommand: Spy<typeof upgradeCommand>;
+  listCommand: Spy<typeof listCommand>;
 }
 
 export interface Task {
